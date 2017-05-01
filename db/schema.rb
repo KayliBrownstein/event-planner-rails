3fe840117d6f073b11e2bf58498f6b7172329398
KayliBrownstein <kaylivbrownstein@gmail.com>
2017-05-01 11:24:38 -0400

    wip

--- db/schema.rb
+++ db/schema.rb
@@ -66,15 +66,13 @@ ActiveRecord::Schema.define(version: 20170501002722) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "name"
-    t.string   "oauth_token"
-    t.datetime "oauth_expires_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
     t.string   "email"
     t.string   "password_digest"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
-Event.create(name: 'Jane and Bob\'s 50th Anniversary', description: 'We should get together for dinner to celebrate their 50th!', cutoff_time: 'Monday')
+Event.create!(name: 'Jane and Bob\'s 50th Anniversary', description: 'We should get together for dinner to celebrate their 50th!', cutoff_time: 'Monday', date: 'Tuesday', time: '6:00PM', location: '229 Tremont Street')
+Event.create!(name: 'Matt\'s 1st Birthday Party', description: 'It\'s his special day!', cutoff_time: 'Sunday', date: 'Saturday', time: '7:00PM', location: '77 Summer Street')
+Event.create!(name: 'Launch\'s Holiday Party', description: 'Our annual work holiday party.', cutoff_time: 'Sunday', date: 'Wednesday', time: '5:00PM', location: '100 Winter Street')
 
-Event.create(name: 'Matt\'s 1st Birthday Party', description: 'It\'s his special day!', cutoff_time: 'Sunday')
-
-Event.create(name: 'Launch\'s Holiday Party', description: 'Our annual work holiday party.', cutoff_time: 'Sunday')
-
-User.create!(name: 'kbrown', email: 'brown@aol.com', password: '123456')
-User.create!(name: 'kbrown2', email: 'crown@aol.com', password: '123456')
-User.create!(name: 'kbrown3', email: 'drown@aol.com', password: '123456')
+User.create!(first_name: 'Bob', last_name: 'Barley', username: 'kbrown', email: 'brown@aol.com', password: '123456')
+User.create!(first_name: 'Kate', last_name: 'Kale', username: 'kbrown2', email: 'crown@aol.com', password: '123456')
+User.create!(first_name: 'Emily', last_name: 'Emu', username: 'kbrown3', email: 'drown@aol.com', password: '123456')
